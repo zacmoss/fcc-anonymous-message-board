0f3b9e168ed8f61a54becfe09fb47e0df80e3d11
Glitch (juicy-cell) <none>
2018-09-30 18:57:23 +0000

    🐴👆 Checkpoint ./routes/api.js:1744758/196

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,10 @@ module.exports = function (app) {
   app.route('/api/threads/:board');
   
   // post text and delete_password
+  
+  // res.redirect to /b/{board}
+  // save _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on),
+  // reported(boolean), delete_password, replies(array)
     
   app.route('/api/replies/:board');
 
