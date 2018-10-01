a5a050da48587c2de7a957e386c88f9a21a44c4f
Glitch (juicy-cell) <none>
2018-10-01 19:23:58 +0000

    🏭🏟 Checkpoint ./routes/api.js:1744758/976

--- routes/api.js
+++ routes/api.js
@@ -32,16 +32,7 @@ module.exports = function (app) {
           res.send({error: 'No board under that name exists'});
         }
       });
-                  
-
-      /*
-      MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true }, function(err, db) {
-                  let dbo = db.db("fcc-cert6-project5");
-                  if (!dbo.collection(messageBoard)) dbo.createCollection(messageBoard);
-                  let collection = dbo.collection(messageBoard);
-                  collection.insertOne({key: 'value test'});
-      });
-      */
+      
     })
   
   
@@ -67,20 +58,34 @@ module.exports = function (app) {
       if (!dbo.collection(messageBoard)) dbo.createCollection(messageBoard);
       let collection = dbo.collection(messageBoard);
       collection.insertOne(dataObject);
-      res.redirect('/api/b/' + messageBoard);
+      res.redirect('/b/' + messageBoard);
     });
     
   })
-  
-  // res.redirect to /b/{board}
-  
+    
   // PUT
   
   // DELETE
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
   
   // GET
+  .get(function(res, req) {
+    let messageBoard = req.params.board;
+    
+    MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true }, function(err, db) {
+        let dbo = db.db("fcc-cert6-project5");
+        if (dbo.collection(messageBoard)) {
+          let collection = dbo.collection(messageBoard);
+          collection.find().toArray(function(err, result) {
+          res.send(result);
+        });
+        } else {
+          res.send({error: 'No board under that name exists'});
+        }
+      });
+    
+  })
   
   // POST
   
