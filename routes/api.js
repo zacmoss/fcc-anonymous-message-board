d7e986e0dbee6f891067b563ef890dc7385455e9
Glitch (juicy-cell) <none>
2018-10-01 18:51:37 +0000

    👹🎱 Checkpoint ./routes/api.js:1744758/1583

--- routes/api.js
+++ routes/api.js
@@ -39,11 +39,33 @@ module.exports = function (app) {
                   collection.insertOne({key: 'value test'});
       });
       */
-    });
+    })
   
   
   // POST
   // post text and delete_password
+  
+  // board text delete_password
+  
+  .post(function(req, res) {
+    let messageBoard = req.params.board;
+    let board = req.query.board;
+    let text = req.query.text;
+    let delete_password = req.query.password;
+    console.log(board);
+    console.log(text);
+    console.log(delete_password);
+    /*
+    MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true }, function(err, db) {
+      let dbo = db.db("fcc-cert6-project5");
+      if (!dbo.collection(messageBoard)) dbo.createCollection(messageBoard);
+      let collection = dbo.collection(messageBoard);
+      //collection.insertOne({key: 'value test'});
+    });
+    */
+  })
+  
+  
   
   // res.redirect to /b/{board}
   // save _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on),
